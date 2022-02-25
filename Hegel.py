133088b7b4ac420a3f5b58397c277d8dedb06674
Beatrice <beatrice.federici123@gmail.com>
2022-02-25 16:42:30 +0000

    created gen text function

--- Hegel.py
+++ Hegel.py
@@ -11,16 +11,20 @@ from keras.layers import LSTM
 from keras.layers import Dropout
 
 
+print(os.listdir("checkpoint"))
+latest = tf.train.latest_checkpoint("checkpoint")
+print(latest)
+
 #read the data:
 #process text
 text = open("Hegel.txt", "rb").read().decode(encoding="utf-8")
 vocab =  sorted(set(text))
-print(f"Length of text: {len(text)}")
-print(f"{len(vocab)}unique characters")
+#print(f"Length of text: {len(text)}")
+#print(f"{len(vocab)}unique characters")
 
 
 #printing the first 250 characters of thet text:
-print(text[:250])
+#print(text[:250])
 
 
 #vectorising the text:
@@ -36,7 +40,7 @@ idx2char =  np.array(vocab)
 text_as_int =  np.array([char2idx[c] for c in text])
 
 #show how the first 13 characters from the text are mapped to integers
-print(f" {repr(text[1:13])} --- characters mapped to int -----> {repr(text_as_int[:13])}")
+#print(f" {repr(text[1:13])} --- characters mapped to int -----> {repr(text_as_int[:13])}")
 
 
 #creating training examples and targets:
@@ -48,8 +52,8 @@ examples_per_epoch = len(text)//(seq_length + 1)
 #creating training examples / targets
 char_dataset = tf.data.Dataset.from_tensor_slices(text_as_int)
 
-for i in char_dataset.take(5):
-    print(idx2char[i.numpy()])
+#for i in char_dataset.take(5):
+ #   print(idx2char[i.numpy()])
 
 
 
@@ -61,8 +65,8 @@ for i in char_dataset.take(5):
 
 sequences = char_dataset.batch(seq_length + 1, drop_remainder=True)
 
-for item in sequences.take(5):
-    print(repr("".join(idx2char[item.numpy()])))
+#for item in sequences.take(5):
+ #   print(repr("".join(idx2char[item.numpy()])))
 
 
 #implementing batches to convert individual characters to sequences of desired sizes:
@@ -78,9 +82,9 @@ dataset = sequences.map(split_input_target)
 
 #print the example of input and example of target
 
-for input_example, target_example in dataset.take(1):
-    print("input data:", repr("".join(idx2char[input_example.numpy()])))
-    print("target data:", repr("".join(idx2char[target_example.numpy()])))
+#for input_example, target_example in dataset.take(1):
+ #   print("input data:", repr("".join(idx2char[input_example.numpy()])))
+  #  print("target data:", repr("".join(idx2char[target_example.numpy()])))
 
 
 
@@ -117,7 +121,7 @@ model = build_model(vocab_size=len(vocab), embedding_dim=embedding_dim, rnn_unit
 
 for input_example_batch, target_example_batch in dataset.take(1):
     example_batch_predictions =  model(input_example_batch)
-    print(example_batch_predictions.shape, "batch size, seq length, vocab size")
+    #print(example_batch_predictions.shape, "batch size, seq length, vocab size")
 
 
 #creating sample indices for the first example batch
@@ -127,8 +131,8 @@ sampled_indices = tf.random.categorical(example_batch_predictions[0], num_sample
 #Use squeeze to discard the dimensions of length one out of the shape of a stated
 sampled_indices = tf.squeeze(sampled_indices, axis=1).numpy()
 
-print("input:", repr("".join(idx2char[input_example_batch[0]])))
-print("input:", repr("".join(idx2char[sampled_indices])))
+#print("input:", repr("".join(idx2char[input_example_batch[0]])))
+#print("input:", repr("".join(idx2char[sampled_indices])))
 
 
 #training the model
@@ -137,8 +141,8 @@ def loss(labels, logits):
     return tf.keras.losses.sparse_categorical_crossentropy(labels, logits, from_logits=True)
 
 example_batch_loss = loss(target_example_batch, example_batch_predictions)
-print("prediction shape:", example_batch_predictions.shape,  "batch size, seq length, vocab size")
-print("scaler_loss", example_batch_loss.numpy().mean())
+#print("prediction shape:", example_batch_predictions.shape,  "batch size, seq length, vocab size")
+#print("scaler_loss", example_batch_loss.numpy().mean())
 
 
 
@@ -163,17 +167,58 @@ log_csv = tf.keras.callbacks.CSVLogger("logs.csv", separator=",", append=False)
 # Model weights are saved at the end of every epoch, if and only if the model is the best seen
 # so far. These models weights will located in the "checkpoint <epoch number>" directories, which are all
 # subdirectories of the "checkpoint directory"
-model.compile(optimizer="adam",loss=loss)
+#model.compile(optimizer="adam",loss=loss)
 
 
-history = model.fit(dataset, epochs=Epochs, callbacks=[checkpoint_callback, early_stop, log_csv])
+#history = model.fit(dataset, epochs=Epochs, callbacks=[checkpoint_callback, early_stop, log_csv])
 
 model.save("Hegel_Augmented_model.h5")
+print(model.summary())
 # The model weights (that are considered the best) are loaded into the model.
-#model.load_weights(checkpoint_filepath)
 
 
 #generating text using the build model
 
-#model = build_model(len(vocab), embedding_dim, rnn_units, batch_size=1)
-#model.build(tf.TensorShape(1))
+model = build_model(len(vocab), embedding_dim, rnn_units, batch_size=1)
+#model.load_weights(tf.train.latest_checkpoint("checkpoint/checkpoint 53"))
+
+#model.build(tf.TensorShape([1, None]))
+
+
+
+def Hegel(model, start_string):
+    #generating the next step using the learned model
+    num_generate = 1000
+
+    #converting start string to nubers (vectorizing)
+    input_eval = [char2idx[s] for s in start_string]
+    input_eval = tf.expand_dims(input_eval, 0)
+
+    #empty list to store results
+    text_generated=[]
+
+    temp = 1.0
+
+    #batch size ==1
+    model.reset_states()
+
+    for i in range(num_generate):
+        predictions = model(input_eval)
+        #remove batch dim
+        predictions = tf.squeeze(predictions, 0)
+
+
+        #use cat dist to predict char returned by model
+
+        predictions = predictions/temp
+        predicted_id = tf.random.categorical(predictions, num_samples=1)[-1,0].numpy()
+
+        #pass the predicted character as the next input to the model
+        input_eval = tf.expand_dims([predicted_id], 0)
+        text_generated.append(idx2char[predicted_id])
+
+    return (start_string + " ".join(text_generated))
+
+
+
+print(Hegel(model,start_string = "Spirit" ))
